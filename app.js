406f99671187a940009bd5f29b5546a118e34dfc
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-07-03 11:56:00 -0400

    d
    d

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const selecttag=document.querySelectorAll("select")
 const button=document.querySelector("button")
 const textbox=document.getElementById("Translated")
+const translatetox=document.getElementById("Translate to")
+const exchange=document.querySelector(".exchange")
 
-
-selecttag.forEach(tag =>{
+selecttag.forEach((tag) =>{
     for (const key in countries){
+       
         let option=`<option value=${key}>${countries[key]}</option>`;
         tag.insertAdjacentHTML("beforeend",option)
+
 
 
 
@@ -20,10 +23,23 @@ button.addEventListener("click",()=>{
     apiurl=`https://api.mymemory.translated.net/get?q=${text}&langpair=${translatefrom}|${translateto}`
     fetch(apiurl).then(res=>
         res.json()).then(data=>{
+            translatetox.value = data.responseData.translatedText;
+
             
 
         })
 })
+
+exchange.addEventListener("click",()=>{
+    temptext=textbox.value
+    templang=selecttag[0].value
+    selecttag[0].value=selecttag[1].value
+    selecttag[1].value=templang
+
+
+    textbox.value=translatetox.value
+    translatetox.value=temptext
+})
     console.log(apiurl)
 
-// continue at 20:00
+// continue at 24:00
