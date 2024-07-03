9e32690b370a105d7e51d9c3556bf1d029c31771
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-07-03 11:32:57 -0400

    hh
    jh

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const selecttag=document.querySelectorAll("select")
+const button=document.querySelector("button")
+const textbox=document.getElementById("Translated")
+
 
 selecttag.forEach(tag =>{
     for (const key in countries){
@@ -10,4 +13,17 @@ selecttag.forEach(tag =>{
     }
 })
 
-// continue at 16:30
+button.addEventListener("click",()=>{
+    let text=textbox.value
+    translatefrom=selecttag[0].value
+    translateto=selecttag[1].value
+    apiurl=`https://api.mymemory.translated.net/get?q=${text}&langpair=${translatefrom}|${translateto}`
+    fetch(apiurl).then(res=>
+        res.json()).then(data=>{
+            
+
+        })
+})
+    console.log(apiurl)
+
+// continue at 20:00
