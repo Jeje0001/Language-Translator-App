9f20929ce33116fcd1fda7b38b38df835cb97d3a
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-07-05 12:31:28 -0400

    jj
    j

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const button=document.querySelector("button")
 const textbox=document.getElementById("Translated")
 const translatetox=document.getElementById("Translate to")
 const exchange=document.querySelector(".exchange")
-
+const icons=document.querySelectorAll(".icons i")
+console.log(icons)
 selecttag.forEach((tag) =>{
     for (const key in countries){
        
@@ -20,11 +21,15 @@ button.addEventListener("click",()=>{
     let text=textbox.value
     translatefrom=selecttag[0].value
     translateto=selecttag[1].value
+    if(!text) return;
+    translatetox.setAttribute("placeholder","Translating")
+
+
     apiurl=`https://api.mymemory.translated.net/get?q=${text}&langpair=${translatefrom}|${translateto}`
     fetch(apiurl).then(res=>
         res.json()).then(data=>{
             translatetox.value = data.responseData.translatedText;
-
+            translatetox.setAttribute("placeholder","Translating")
             
 
         })
@@ -40,6 +45,33 @@ exchange.addEventListener("click",()=>{
     textbox.value=translatetox.value
     translatetox.value=temptext
 })
-    console.log(apiurl)
 
-// continue at 24:00
+icons.forEach(icon=>{
+    icon.addEventListener("click",({target})=>{
+        console.log(target.id)
+        if(target.classList.contains("fa-copy")){
+            if(target.id === "from"){
+                navigator.clipboard.writeText(textbox.value)
+                console.log("From copy clicked")
+            }else{
+                navigator.clipboard.writeText(translatetox.value)
+
+                console.log("To copy icon clicked")
+
+            }
+        }else{
+            if(target.id === "from"){utterance
+                utterance= new SpeechSynthesisUtterance(textbox.value)
+                utterance.lang=selecttag[0].value
+
+            }else{
+                utterance= new SpeechSynthesisUtterance(translatetox.value)
+                utterance.lang=selecttag[1].value
+
+
+            }
+            speechSynthesis.speak(utterance)
+        }
+    })
+})
+
